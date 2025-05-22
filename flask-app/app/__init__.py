5c26272a7dce02798509d0dea9e684a7fb239f01
Samuel Chan <ccaecha@ucl.ac.uk>
2025-05-22 13:21:21 +0100

    api

--- flask-app/app/__init__.py
+++ flask-app/app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from .main import routes as main_blueprint
+from .main import api as api_blueprint
 
 def create_app():
     app = Flask(__name__)
     
     app.register_blueprint(main_blueprint.main)
+    app.register_blueprint(api_blueprint.blueprint, url_prefix='/api')
 
     return app
