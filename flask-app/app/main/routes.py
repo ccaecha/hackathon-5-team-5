8fb7fa2b97604301ab3d351819ee186ff6ec6a82
minjnlgc <kunyuquemian@outlook.com>
2025-05-22 12:57:33 +0100

    dashboard view

--- flask-app/app/main/routes.py
+++ flask-app/app/main/routes.py
@@ -2,19 +2,27 @@ from flask import Blueprint, render_template
 from flask import jsonify
 from ..db.db import MockDatabase
 
-main = Blueprint('main', __name__)
+main = Blueprint("main", __name__)
 
-@main.route('/')
+
+@main.route("/")
 def index():
-    return render_template('main/index.html')
+    return render_template("main/index.html")
 
-@main.route('/get-data')
+
+@main.route("/get-data")
 def get_data():
     return jsonify({"message": "This is dummy data", "status": "success"})
 
-@main.route('/db-test')
+
+@main.route("/dashboard")
+def dashboard():
+    return render_template("main/dashboard.html")
+
+
+@main.route("/db-test")
 def db_test():
-    MockDatabase.insert('test_table', {'id': 1, 'name': 'Test'})
-    MockDatabase.get_all('test_table')
-    data = MockDatabase.get_all('test_table')
+    MockDatabase.insert("test_table", {"id": 1, "name": "Test"})
+    MockDatabase.get_all("test_table")
+    data = MockDatabase.get_all("test_table")
     return jsonify(data)
