ae7785a5390f127d287657acd491463f9c87fe42
Samuel Chan <ccaecha@ucl.ac.uk>
2025-05-22 12:40:09 +0100

    db demo

--- flask-app/app/main/routes.py
+++ flask-app/app/main/routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template
 from flask import jsonify
+from ..db.db import MockDatabase
 
 main = Blueprint('main', __name__)
 
@@ -10,3 +11,10 @@ def index():
 @main.route('/get-data')
 def get_data():
     return jsonify({"message": "This is dummy data", "status": "success"})
+
+@main.route('/db-test')
+def db_test():
+    MockDatabase.insert('test_table', {'id': 1, 'name': 'Test'})
+    MockDatabase.get_all('test_table')
+    data = MockDatabase.get_all('test_table')
+    return jsonify(data)
